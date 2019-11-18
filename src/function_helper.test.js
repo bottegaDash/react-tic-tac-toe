97d99186f7b7d981c23355d3e7fee4ae21256dfa
pillar-david-j-ash <david.j.ash@accenture.com>
2019-11-18 08:26:06 -0500

    Added tests for square function

---
+++ src/function_helper.test.js
@@ -0,0 +1,30 @@
+import { Square } from "./function_helper.js";
+// setup file
+import { configure } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+
+configure({ adapter: new Adapter() });
+import { shallow } from "enzyme";
+
+describe("Test Square", () => {
+  const mockCallBack = jest.fn();
+  var props = { onClick: mockCallBack, value: "O" };
+  var squareButton = shallow(Square(props));
+
+  it("Contains button element", () => {
+    expect(squareButton.exists("button")).toEqual(true);
+  });
+
+  it("Contains a class name of square", () => {
+    expect(squareButton.exists(".square")).toEqual(true);
+  });
+
+  it("Contains a clickEvent", () => {
+    squareButton.find("button").simulate("click");
+    expect(mockCallBack.mock.calls.length).toEqual(1);
+  });
+
+  it("Contains a content in the button tag", () => {
+    expect(squareButton.text()).toEqual("O");
+  });
+});
