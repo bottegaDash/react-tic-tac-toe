4eb46c42a0c5bf386880daf1b616b2e615dbb8a2
pillar-david-j-ash <david.j.ash@accenture.com>
2019-11-18 07:35:01 -0500

    changed p to o

--- src/index.js
+++ src/index.js
@@ -24,7 +24,7 @@ class Board extends React.Component {
     if (calculateWinner(squares) || squares[i]) {
       return;
     }
-    squares[i] = this.state.xIsNext ? "X" : "P";
+    squares[i] = this.state.xIsNext ? "X" : "O";
     this.setState({
       squares: squares,
       xIsNext: !this.state.xIsNext
